a9cb3cee713cc577a79cc440ce26c6389c66ca2f
lover <wssl135@163.com>
2018-07-10 08:09:16 +0800

    edit sidebar-menu

--- resources/views/partials/sidebar-menu.blade.php
+++ resources/views/partials/sidebar-menu.blade.php
@@ -5,7 +5,7 @@
     <li>
         <a class="sidebar-list-title block" data-toggle="collapse" data-target="#eloquent">门锁</a>
         <div id="eloquent" class="collapse collapse-top in">
-            <ul class="list-sub-items">
+            <ul class="list-sub-items" style="padding-left: 10px;">
                 <li><a class="keyword {!! Request::is('eloquent/l0102-locker') ? 'active' : '' !!}" href="{!! url('eloquent/l0102-locker') !!}">L-01/02门锁</a></li>
                 <li><a class="keyword {!! Request::is('eloquent/l21-locker') ? 'active' : '' !!}" href="{!! url('eloquent/l21-locker') !!}">L-21门锁</a></li>
                 <li><a class="keyword {!! Request::is('eloquent/hw2802-locker') ? 'active' : '' !!}" href="{!! url('eloquent/hw2802-locker') !!}">HW-2802防水摇把锁</a></li>
@@ -17,7 +17,7 @@
     <li>
         <a class="sidebar-list-title block" data-toggle="collapse" data-target="#shield">屏蔽材料</a>
         <div id="shield" class="collapse collapse-top in">
-            <ul class="list-sub-items">
+            <ul class="list-sub-items" style="padding-left: 10px;">
                 <li><a class="keyword {!! Request::is('shield/fabric') ? 'active' : '' !!}" href="{!! url('shield/fabric') !!}">导电布</a></li>
                 <li><a class="keyword {!! Request::is('shield/plate') ? 'active' : '' !!}" href="{!! url('shield/plate') !!}">波导板</a></li>
                 <li><a class="keyword {!! Request::is('shield/finger') ? 'active' : '' !!}" href="{!! url('shield/finger') !!}">屏蔽簧片</a></li>
@@ -27,7 +27,7 @@
     <li>
         <a class="sidebar-list-title block" data-toggle="collapse" data-target="#rubber">导电橡胶</a>
         <div id="rubber" class="collapse collapse-top in">
-            <ul class="list-sub-items">
+            <ul class="list-sub-items" style="padding-left: 10px;">
                 <li><a class="keyword {!! Request::is('rubber/conductive') ? 'active' : '' !!}" href="{!! url('rubber/conductive') !!}">导电布</a></li>
                 <li><a class="keyword {!! Request::is('rubber/fip') ? 'active' : '' !!}" href="{!! url('rubber/fip') !!}">FIP导电胶</a></li>
             </ul>
